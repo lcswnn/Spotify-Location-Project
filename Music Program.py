8c9d3a33817e9c006ac5d1c63bdd53ecbc833623
Lucas Waunn <118494460+lcswnn@users.noreply.github.com>
2024-07-30 15:25:57 -0500

    Uploading Template

---
+++ Music Program.py
@@ -0,0 +1,23 @@
+'''____________________________________________________________________________________
+   Concept: Music changes based on where you are driving and location in the world  
+                                                                                    
+   Lucas Waunn: SWE Side Project                                                    
+____________________________________________________________________________________'''
+
+''' 
+For GPS Tracking: https://www.geeksforgeeks.org/gps-tracker-using-python/
+     Assuming we can just use a loop with a wait-timer and refresh the location
+     while program is running
+
+For Spotify Playing: https://pypi.org/project/pyspotify-connect/
+      Only works with Spotify Premium so should be good
+
+'''
+
+#Libraries
+# Geopy
+# 
+
+#Code
+
+
